ba251e0fbb49ff19b5e8997b60387dc99ea07e86
Dave <davide.dcube@gmail.com>
2023-02-19 22:54:40 +0100

    ✨ Clean code

--- src/native-test/java/red/tetracube/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package red.tetracube;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
