a9589b93b2f15bd8afe40bc87400860883925ed8
Dave <davide.dcube@gmail.com>
2023-02-28 22:15:30 +0100

    🛝 Added validation annotations to the request

--- src/main/java/red/tetracube/guest/payloads/GuestLoginAPIRequest.java
+++ src/main/java/red/tetracube/guest/payloads/GuestLoginAPIRequest.java
@@ -1,12 +1,19 @@
 package red.tetracube.guest.payloads;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.smallrye.common.constraint.NotNull;
+
+import javax.validation.constraints.NotEmpty;
 
 public class GuestLoginAPIRequest {
 
+    @NotNull
+    @NotEmpty
     @JsonProperty("name")
     public String name;
 
+    @NotNull
+    @NotEmpty
     @JsonProperty("password")
     public String password;
 }
